cefb5242dfec282b44438cc6303ced72149145ad
Ravikumar Rajagopalan <ravi.911kumar@gmail.com>
2017-05-02 23:08:28 +0530

    Initial commit for our Spring boot project

---
+++ src/main/java/com/poruki/PorukiApplication.java
@@ -0,0 +1,12 @@
+package com.poruki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PorukiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PorukiApplication.class, args);
+	}
+}
